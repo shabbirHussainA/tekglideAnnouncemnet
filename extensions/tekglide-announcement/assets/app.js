fa6b8628691c23d8c8daae6855b993781269d846
shabbirHussainA <shabbirhussainwork@gmail.com>
2024-11-06 19:46:07 +0500

    added ful functionality

--- extensions/tekglide-announcement/assets/app.js
+++ extensions/tekglide-announcement/assets/app.js
@@ -1,23 +1,62 @@
 
-    let currentIndex = 0;
-    // Slide left functionality for the specific slider
-    window.slideLeft = function(blockId) {
-      const announcements = document.querySelectorAll(`#slider-content-${blockId} .announcement`);
-      if (currentIndex > 0) {
-        currentIndex--;
-      } else {
-        currentIndex = announcements.length - 1;
-      }
-      document.getElementById(`slider-content-${blockId}`).style.transform = `translateX(-${currentIndex * 100}%)`;
-    };
+    // let currentIndex = 0;
+    // // Slide left functionality for the specific slider
+    // window.slideLeft = function(blockId) {
+    //   const announcements = document.querySelectorAll(`#slider-content-${blockId} .announcement`);
+    //   if (currentIndex > 0) {
+    //     currentIndex--;
+    //   } else {
+    //     currentIndex = announcements.length - 1;
+    //   }
+    //   document.getElementById(`slider-content-${blockId}`).style.transform = `translateX(-${currentIndex * 100}%)`;
+    // };
 
-    // Slide right functionality for the specific slider
-    window.slideRight = function(blockId) {
-      const announcements = document.querySelectorAll(`#slider-content-${blockId} .announcement`);
-      if (currentIndex < announcements.length - 1) {
-        currentIndex++;
-      } else {
-        currentIndex = 0;
-      }
-      document.getElementById(`slider-content-${blockId}`).style.transform = `translateX(-${currentIndex * 100}%)`;
-    };
+    // // Slide right functionality for the specific slider
+    // window.slideRight = function(blockId) {
+    //   const announcements = document.querySelectorAll(`#slider-content-${blockId} .announcement`);
+    //   if (currentIndex < announcements.length - 1) {
+    //     currentIndex++;
+    //   } else {
+    //     currentIndex = 0;
+    //   }
+    //   document.getElementById(`slider-content-${blockId}`).style.transform = `translateX(-${currentIndex * 100}%)`;
+    // };
+
+    $('.slide').slick({
+      dots: true,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      dots: false,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
